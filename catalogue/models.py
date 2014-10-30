6548e71ecc719f83b664bfa0f82f7afd4fad2b9c
David Simic <dpsimic@gmail.com>
2014-10-30 14:46:22 -0500

    media

--- catalogue/models.py
+++ catalogue/models.py
@@ -33,7 +33,7 @@ class Product(models.Model):
     name = models.CharField(max_length=300)
     slug = models.SlugField(max_length=150)
     description = models.TextField()
-    photo = models.ImageField(upload_to='product_photo', blank=True)
+    photo = models.ImageField(upload_to='media/product_photo', blank=True)
     manufacturer = models.CharField(max_length=300, blank=True)
     price_in_cop = models.DecimalField(max_digits=6, decimal_places=3)
 
